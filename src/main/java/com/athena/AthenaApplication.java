9bc464478849b4222e72a870dbc1269e1134c408
Josh Williamson <josh.williamson@digital.homeoffice.gov.uk>
2021-02-21 12:04:30 +0000

    code tidy

--- src/main/java/com/athena/AthenaApplication.java
+++ src/main/java/com/athena/AthenaApplication.java
@@ -1,12 +1,7 @@
 package com.athena;
 
-import javax.validation.constraints.Email;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.athena.dal.ConsultantDAL;
 
 @SpringBootApplication
 public class AthenaApplication {
